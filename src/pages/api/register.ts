b933ee3a876606a80b66c8bd7c49825667da2149
Dicky Darmawan <dicky93darmawan@gmail.com>
2023-07-23 04:31:46 +0800

    .

--- src/pages/api/register.ts
+++ src/pages/api/register.ts
@@ -1,34 +1,42 @@
-import { verifyJwt } from "~/utils/jwt";
+import { signJwtAccessToken } from "~/utils/jwt";
 import { prisma } from "~/server/db";
 import { type NextApiRequest, type NextApiResponse } from "next";
-import type { JWT } from "next-auth/jwt";
+import { env } from "~/env.mjs";
+
+interface RequestBody {
+  email: string;
+  image: string;
+  name: string;
+  isActive?: boolean;
+}
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const authToken = (req.headers.authorization || "").split("Bearer ").at(1);
+  const keysLogin = req.headers["authorization"];
+  const {
+    image,
+    email,
+    name,
+    isActive = true,
+  }: RequestBody = req.body as RequestBody;
 
-  if (!authToken) {
-    return res.status(401).json({
-      code: "401",
-      status: "Unauthorized",
-      errors: [{ token: ["Token Null"] }],
-    });
-  }
-
-  const JWT = verifyJwt(authToken) as JWT | undefined;
-  if (!JWT) {
-    return res.status(401).json({
-      code: "401",
-      status: "Unauthorized",
-      errors: [{ token: ["Invalid Auth Token"] }],
-    });
-  }
-
-  const user = await prisma.user.findUnique({
+  const user = await prisma.user.upsert({
     where: {
-      email: JWT.email,
+      email,
+    },
+    create: {
+      email,
+      emailVerified: new Date(),
+      image,
+      isActive,
+      name,
+    },
+    update: {
+      email,
+      image,
+      name,
     },
   });
 
@@ -36,13 +44,28 @@ export default async function handler(
     return res.status(401).json({
       code: "401",
       status: "Unauthorized",
-      errors: [{ user: ["User Not Found"] }],
+      errors: [{ user: ["Akun Anda tidak terdaftar"] }],
     });
   }
+
+  if (user?.isActive !== true) {
+    return res.status(401).json({
+      code: "401",
+      status: "Unauthorized",
+      errors: [{ user: ["Status anda non active"] }],
+    });
+  }
+  
+  const { ...userWithoutPass } = user;
+  const accessToken = signJwtAccessToken(userWithoutPass);
+  const result = {
+    ...userWithoutPass,
+    accessToken,
+  };
 
   return res.status(200).json({
     code: "200",
     status: "Succses",
-    data: user,
+    data: result,
   });
 }
