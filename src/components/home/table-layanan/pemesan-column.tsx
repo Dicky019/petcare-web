a45a410d51458ba04a5345af9380da035412bd8b
Dicky Darmawan <dicky93darmawan@gmail.com>
2023-07-28 19:49:04 +0800

    .

--- src/components/home/table-layanan/pemesan-column.tsx
+++ src/components/home/table-layanan/pemesan-column.tsx
@@ -4,7 +4,11 @@ import { type IPemesananLayanan } from "~/types/pemesanan-layanan";
 import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
 import { Badge } from "~/components/ui/badge";
 import { RowActions } from "./row-action";
-import { HoverCard, HoverCardContent, HoverCardTrigger } from "~/components/ui/hover-card";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "~/components/ui/hover-card";
 import { Button } from "~/components/ui/button";
 import { SiCodereview } from "react-icons/si";
 import { displayJam } from "~/utils/function";
@@ -56,58 +60,58 @@ export const pemesanColumns: ColumnDef<IPemesananLayanan>[] = [
   },
   {
     accessorKey: "namaHewan",
-    header: "Nama Hewan",
+    header: "Hewan",
+    cell: ({ row }) => {
+      const data = row.original;
+      const jenisKelaminHewan = data.jenisKelaminHewan
+        .split("")
+        .map((v, i) => (i === 0 ? v.toUpperCase() : v)).join("");
+      return (
+        <div className="flex flex-col gap-y-1">
+          <div>{data.namaHewan}</div>
+          <div className="text-xs text-gray-500">{ data.umurHewan+ " / " + jenisKelaminHewan}</div>
+        </div>
+      );
+    },
   },
   {
     accessorKey: "tanggal",
     header: "Tanggal",
-    cell: ({ row }) => row.getValue<string>("tanggal").toUpperCase(),
-  },
-  {
-    accessorKey: "jam",
-    header: "Jam",
-    cell: ({ row }) => displayJam(row.getValue<string>("jam")).toUpperCase(),
-  },
-  {
-    accessorKey: "jenisKelaminHewan",
-    header: () => <div className="w-36">Jenis Kelamin Hewan</div>,
     cell: ({ row }) => {
-      const jenisKelaminHewan = row
-        .getValue<string>("jenisKelaminHewan")
-        .split("")
-        .map((v, i) => (i === 0 ? v.toUpperCase() : v));
-      return jenisKelaminHewan;
+      const data = row.original;
+      return (
+        <div className="flex w-24 flex-col gap-y-1">
+          <div>{data.tanggal ?? "shadcn"}</div>
+          <div className="text-xs text-gray-500">
+            {displayJam(data.jam) ?? "shadcn@gmail.com"}
+          </div>
+        </div>
+      );
     },
   },
   {
-    accessorKey: "umurHewan",
-    header: "Umur Hewan",
-  },
-  {
-    accessorKey: "kategoriHewan",
-    header: "Kategori Hewan",
-  },
-  {
     accessorKey: "keluhan",
-    header: () => <div className="text-right">Keluhan</div>,
+    header:  "Keluhan",
     cell: ({ row }) => {
       const keluhan = row.getValue<string>("keluhan");
-      return  (<div className="flex items-center gap-x-2">
-      <div className="text-left max-w-[250px] line-clamp-2 font-medium">
-        {keluhan}
-      </div>
-      <HoverCard>
-        <HoverCardTrigger>
-          <Button size="sm" variant="outline">
-            <SiCodereview />
-          </Button>
-        </HoverCardTrigger>
-        <HoverCardContent className="text-left">
-          <div className="font-semibold mb-1 text-lg">Keluhan</div>
-          <div>{keluhan}</div>
-        </HoverCardContent>
-      </HoverCard>
-    </div>);
+      return (
+        <div className="flex items-center gap-x-2">
+          <div className="line-clamp-2 max-w-[250px] text-left font-medium">
+            {keluhan}
+          </div>
+          <HoverCard>
+            <HoverCardTrigger>
+              <Button size="sm" variant="outline">
+                <SiCodereview />
+              </Button>
+            </HoverCardTrigger>
+            <HoverCardContent className="text-left">
+              <div className="mb-1 text-lg font-semibold">Keluhan</div>
+              <div>{keluhan}</div>
+            </HoverCardContent>
+          </HoverCard>
+        </div>
+      );
     },
   },
   {
--- src/components/home/table-layanan/pemesan-no-keluhan-column.tsx
+++ src/components/home/table-layanan/pemesan-no-keluhan-column.tsx
@@ -53,45 +53,35 @@ export const pemesanNoKeluhanColumns: ColumnDef<IPemesananLayanan>[] = [
   },
   {
     accessorKey: "namaHewan",
-    header: "Nama Hewan",
+    header: "Hewan",
+    cell: ({ row }) => {
+      const data = row.original;
+      const jenisKelaminHewan = data.jenisKelaminHewan
+        .split("")
+        .map((v, i) => (i === 0 ? v.toUpperCase() : v)).join("");
+      return (
+        <div className="flex flex-col gap-y-1">
+          <div>{data.namaHewan}</div>
+          <div className="text-xs text-gray-500">{ data.umurHewan+ " / " + jenisKelaminHewan}</div>
+        </div>
+      );
+    },
   },
   {
     accessorKey: "tanggal",
     header: "Tanggal",
-    cell: ({ row }) => row.getValue<string>("tanggal").toUpperCase(),
-  },
-  {
-    accessorKey: "jam",
-    header: "Jam",
-    cell: ({ row }) => displayJam(row.getValue<string>("jam")).toUpperCase(),
-  },
-  {
-    accessorKey: "jenisKelaminHewan",
-    header: () => <div className="w-36">Jenis Kelamin Hewan</div>,
     cell: ({ row }) => {
-      const jenisKelaminHewan = row
-        .getValue<string>("jenisKelaminHewan")
-        .split("")
-        .map((v, i) => (i === 0 ? v.toUpperCase() : v));
-      return jenisKelaminHewan;
+      const data = row.original;
+      return (
+        <div className="flex w-24 flex-col gap-y-1">
+          <div>{data.tanggal ?? "shadcn"}</div>
+          <div className="text-xs text-gray-500">
+            {displayJam(data.jam) ?? "shadcn@gmail.com"}
+          </div>
+        </div>
+      );
     },
   },
-  {
-    accessorKey: "umurHewan",
-    header: "Umur Hewan",
-  },
-  {
-    accessorKey: "kategoriHewan",
-    header: "Kategori Hewan",
-  },
-  // {
-  //   accessorKey: "keluhan",
-  //   header: () => <div className="text-right">Keluhan</div>,
-  //   cell: ({ row }) => {
-  //     const jenisKelaminHewan = row.getValue<string>("keluhan");
-  //     return jenisKelaminHewan;
-  //   },
-  // },
   {
     id: "actions",
     cell: ({ row }) => <RowActions pemesananLayanan={row.original} />,
