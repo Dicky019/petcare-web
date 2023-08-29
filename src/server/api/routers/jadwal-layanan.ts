4369e4217134a4a19cc2ae35dd852b6d879284d8
Dicky Darmawan <dicky93darmawan@gmail.com>
2023-08-29 19:12:54 +0800

    .

--- src/server/api/routers/jadwal-layanan.ts
+++ src/server/api/routers/jadwal-layanan.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 import { getAllJadwalLayanan, validated } from "~/service/jadwal-layanan";
 import { formCreateSchema, formEditSchema } from "~/types/jadwal-layanan";
+import { listHari } from "~/utils/data";
 
 export const jadwalLayananRouter = createTRPCRouter({
   getAll: protectedProcedure.query(async ({ ctx }) => {
@@ -11,10 +12,29 @@ export const jadwalLayananRouter = createTRPCRouter({
         prisma: ctx.prisma,
       });
 
+    const filterByHari = (hari: string) => {
+      const haris = Object.entries(listHari);
+      console.log({ haris, hari });
+      const hariKey = haris.find((h) => h[1] === hari)?.[0];
+      return hariKey ?? "0";
+    };
+
     return {
-      layananGrouming: allLayananGrooming,
-      layananKesehatan: allLayananKesehatan,
-      layananKonsultasi: allLayananKonsultasi,
+      layananGrouming: allLayananGrooming.sort((a,b) => {
+        const hariA = filterByHari(a.hari)
+        const hariB = filterByHari(b.hari)
+        return Number(hariA) - Number(hariB);
+      }),
+      layananKesehatan: allLayananKesehatan.sort((a,b) => {
+        const hariA = filterByHari(a.hari)
+        const hariB = filterByHari(b.hari)
+        return Number(hariA) - Number(hariB);
+      }),,
+      layananKonsultasi: allLayananKonsultasi.sort((a,b) => {
+        const hariA = filterByHari(a.hari)
+        const hariB = filterByHari(b.hari)
+        return Number(hariA) - Number(hariB);
+      }),
     };
   }),
 
@@ -40,29 +60,31 @@ export const jadwalLayananRouter = createTRPCRouter({
         },
       });
     }),
-  edit: protectedProcedure.input(formEditSchema).mutation(async ({ ctx, input }) => {
-    const jenisLayanan = input.jenisLayanan as JenisLayanan;
-    const hari = input.hari as Hari;
-    const jam = input.jam;
+  edit: protectedProcedure
+    .input(formEditSchema)
+    .mutation(async ({ ctx, input }) => {
+      const jenisLayanan = input.jenisLayanan as JenisLayanan;
+      const hari = input.hari as Hari;
+      const jam = input.jam;
 
-    await validated({
-      prisma: ctx.prisma,
-      hari,
-      jenisLayanan,
-      jam,
-    });
-
-    return ctx.prisma.jadwalLayanan.update({
-      data: {
+      await validated({
+        prisma: ctx.prisma,
+        hari,
         jenisLayanan,
         jam,
-        hari,
-      },
-      where: {
-        id: input.id,
-      },
-    });
-  }),
+      });
+
+      return ctx.prisma.jadwalLayanan.update({
+        data: {
+          jenisLayanan,
+          jam,
+          hari,
+        },
+        where: {
+          id: input.id,
+        },
+      });
+    }),
   delete: protectedProcedure.input(z.string()).mutation(({ ctx, input }) => {
     return ctx.prisma.jadwalLayanan.delete({
       where: {
